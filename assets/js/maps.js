829eb57cc84245150fee782996d1d2a22c972b21
TomokokomoT <tomokoaota@gmail.com>
2020-06-09 20:21:15 +0000

    Adding Google Maps API

---
+++ assets/js/maps.js
@@ -0,0 +1,20 @@
+@return void
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 3,
+    center: {
+      lat: 46.619261,
+      lng: -33.134766,
+    },
+  });
+
+  var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  var markers = locations.map(function(location, i) {
+          return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+          });
+        });
+}
