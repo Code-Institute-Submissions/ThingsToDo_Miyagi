fa6cf35222139529ad908606a52d20a338304c37
TomokokomoT <tomokoaota@gmail.com>
2020-06-18 22:51:12 +0000

    fixing google map

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,21 +1,25 @@
-var map;
-var service;
-var infowindow;
+// marker clustering //
 
 function initMap() {
-  infowindow = new google.maps.InfoWindow();
 
-  map = new google.maps.Map(document.getElementById("map"), {
+  var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 8,
     center: {lat: 38.6306, lng: 141.1193}
   });
-  
-  var request = {
-          location: location,
-          fields: ['name', 'geometry'],
-        };
-  
-  var locations = [
+
+  var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  var markers = locations.map(function(location, i) {
+          return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+          });
+        });
+
+  var markerCluster = new MarkerClusterer(map, markers,
+            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+}
+ var locations = [
       {lat: 38.1365, lng: 140.4495},
       {lat: 38.9608, lng: 140.7882},
       {lat: 38.7296, lng: 140.6908},
@@ -27,33 +31,7 @@ function initMap() {
       {lat: 38.4188, lng: 140.7230}
   ]
 
-  service = new google.maps.places.PlacesService(map);
-
-   service.findPlaceFromQuery(request, function(results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      for (var i = 0; i < results.length; i++) {
-        createMarker(results[i]);
-      }
-
-      map.setCenter(results[0].geometry.location);
-    }
-  });
-}
-        
- function createMarker(place) {
-  var marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location
-  });
-
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(place.name);
-    infowindow.open(map, this);
-  });
-}
-
-
-// Filter table: taken from https://www.w3schools.com/howto/howto_js_filter_table.asp 
+// Filter table: taken from https://www.w3schools.com/howto/howto_js_filter_table.asp //
 function myFunction() {
   var input, filter, table, tr, td, i, txtValue;
   input = document.getElementById("myInput");
