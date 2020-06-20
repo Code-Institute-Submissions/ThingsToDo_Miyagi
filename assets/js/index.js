07ce0ffeb8eff7c52d66898aa6b7974e240c478c
TomokokomoT <tomokoaota@gmail.com>
2020-06-20 17:01:23 +0000

    Moved JS codes from index.html

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+// taken from w3schools.com/how to add active class to current element
+		var header = document.getElementById("navbarNavDropdown");
+        var btns = header.getElementsByClassName("nav-link");
+        for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+        });
+        }
+
+// taken from https://stackoverflow.com/questions/19469705/bootstrap-carousel-as-website-background-->
+       $(document).ready(function() {
+       $('.carousel').carousel({interval: 5000});
+     });
