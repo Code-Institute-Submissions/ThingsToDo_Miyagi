4af3fb0de5331cdb2762e1f225b4e0f2545f0c46
TomokokomoT <tomokoaota@gmail.com>
2020-06-20 12:16:39 +0000

    adiing markers with infowindows in teh google map

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,8 +1,6 @@
-// marker clustering //
-
+// marker clustering 
 function initMap() {
-
-  var map = new google.maps.Map(document.getElementById("map"), {
+ var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 8,
     center: {lat: 38.6306, lng: 141.1193}
   });
@@ -18,20 +16,39 @@ function initMap() {
 
   var markerCluster = new MarkerClusterer(map, markers,
             {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
-}
- var locations = [
-      {lat: 38.1365, lng: 140.4495},
-      {lat: 38.9608, lng: 140.7882},
-      {lat: 38.7296, lng: 140.6908},
-      {lat: 38.5889, lng: 141.0984},
-      {lat: 38.4717, lng: 140.8864},
-      {lat: 38.1853, lng: 140.6719},
-      {lat: 38.1267, lng: 140.5287},
-      {lat: 38.7869, lng: 140.6394},
-      {lat: 38.4188, lng: 140.7230}
-  ]
 
-// Filter table: taken from https://www.w3schools.com/howto/howto_js_filter_table.asp //
+   // infoWindows 
+   var  infowindow = new google.maps.InfoWindow(),  marker, i;
+
+   function createMarker(place) {
+      var markers = locations.map(function(location, i) {
+          marker =  new google.maps.Marker({
+            position: location.marker,
+            label: labels[i % labels.length]
+          });
+
+          google.maps.event.addListener(marker, 'click', function() {
+              infowindow.setContent('<div><strong>' + location.description + '</strong><br></div>');
+              infowindow.open(map, this);
+            });
+          return marker;
+         }); 
+    }
+}
+
+var locations = [
+     {marker: {lat: 38.1365, lng: 140.4495}, description: "Kurikomayama"}, 
+     {marker: {lat: 38.9608, lng: 140.7882}, description: "Naruko Gorge"},
+     {marker: {lat: 38.7296, lng: 140.6908}, description: "Zao Okama"},
+     {marker: {lat: 38.5889, lng: 141.0984}, description: "Kagobo Park Golf Course"},
+     {marker: {lat: 38.4717, lng: 140.8864}, description: "Manyo Create Park"},
+     {marker: {lat: 38.1853, lng: 140.6719}, description: "Michinoku Park"},
+     {marker: {lat: 38.1267, lng: 140.5287}, description: "Onikuobe"},
+     {marker: {lat: 38.7869, lng: 140.6394}, description: "Spring Valley"},
+     {marker: {lat: 38.4188, lng: 140.7230}, description: "Zao Eboshi Ski Resort"}
+  ];
+
+// Filter table: taken from https://www.w3schools.com/howto/howto_js_filter_table.asp 
 function myFunction() {
   var input, filter, table, tr, td, i, txtValue;
   input = document.getElementById("myInput");
@@ -40,15 +57,15 @@ function myFunction() {
   tr = table.getElementsByTagName("tr");
 
   for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
-    if (td) {
-      txtValue = td.textContent || td.innerText;
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+         txtValue = td.textContent || td.innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
         tr[i].style.display = "";
-      } else {
+        } else {
         tr[i].style.display = "none";
-      }
+       }
     }
-  }
+
 }
-    
+}
