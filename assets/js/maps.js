a2c256dc3a6fde8b9c2a58577e539109d63ad403
TomokokomoT <tomokoaota@gmail.com>
2020-06-21 00:25:20 +0000

    Trying to add infowindows

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -14,18 +14,30 @@ function initMap() {
         });
  });
 
-  var markerCluster = new MarkerClusterer(map, markers,
+ var locations = [
+     {marker: {lat: 38.1365, lng: 140.4495}, description: "Kurikomayama"}, 
+     {marker: {lat: 38.9608, lng: 140.7882}, description: "Naruko Gorge"},
+     {marker: {lat: 38.7296, lng: 140.6908}, description: "Zao Okama"},
+     {marker: {lat: 38.5889, lng: 141.0984}, description: "Kagobo Park Golf Course"},
+     {marker: {lat: 38.4717, lng: 140.8864}, description: "Manyo Create Park"},
+     {marker: {lat: 38.1853, lng: 140.6719}, description: "Michinoku Park"},
+     {marker: {lat: 38.1267, lng: 140.5287}, description: "Onikuobe"},
+     {marker: {lat: 38.7869, lng: 140.6394}, description: "Spring Valley"},
+     {marker: {lat: 38.4188, lng: 140.7230}, description: "Zao Eboshi Ski Resort"}
+];
+
+ var markerCluster = new MarkerClusterer(map, markers,
           {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
 
    // infoWindows 
    var  infowindow = new google.maps.InfoWindow();
 
    var request = {
-          query: 'Sendai',
-          fields: ['name', 'geometry'],
-        };
+    location: location,
+    fields: ['name', 'geometry'],
+  };
 
-        service = new google.maps.places.PlacesService(map);
+   var service = new google.maps.places.PlacesService(map);
 
         service.findPlaceFromQuery(request, function(results, status) {
           if (status === google.maps.places.PlacesServiceStatus.OK) {
@@ -54,22 +66,10 @@ function initMap() {
     }
 }
 
-var locations = [
-     {marker: {lat: 38.1365, lng: 140.4495}, description: "Kurikomayama"}, 
-     {marker: {lat: 38.9608, lng: 140.7882}, description: "Naruko Gorge"},
-     {marker: {lat: 38.7296, lng: 140.6908}, description: "Zao Okama"},
-     {marker: {lat: 38.5889, lng: 141.0984}, description: "Kagobo Park Golf Course"},
-     {marker: {lat: 38.4717, lng: 140.8864}, description: "Manyo Create Park"},
-     {marker: {lat: 38.1853, lng: 140.6719}, description: "Michinoku Park"},
-     {marker: {lat: 38.1267, lng: 140.5287}, description: "Onikuobe"},
-     {marker: {lat: 38.7869, lng: 140.6394}, description: "Spring Valley"},
-     {marker: {lat: 38.4188, lng: 140.7230}, description: "Zao Eboshi Ski Resort"}
-  ]
-
 // Filter table: taken from https://www.w3schools.com/howto/howto_js_filter_table.asp 
 function myFunction() {
   var input, filter, table, tr, td, i, txtValue;
-  input = document.getElementById("myInput");
+  input = document.getElementById("searchActivities");
   filter = input.value.toUpperCase();
   table = document.getElementById("activitiesTable");
   tr = table.getElementsByTagName("tr");
@@ -84,6 +84,5 @@ function myFunction() {
         tr[i].style.display = "none";
        }
     }
-
-}
+  }
 }
