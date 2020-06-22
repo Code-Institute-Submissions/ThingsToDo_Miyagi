1395167fc5f32c8c45ed6c754485fdbdd71d49e3
TomokokomoT <tomokoaota@gmail.com>
2020-06-22 20:57:29 +0000

    Removed the codes for infowindow

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,76 +1,38 @@
 function initMap() {
- var map = new google.maps.Map(document.getElementById("map"), {
+  var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 8,
-    center: {lat: 38.6306, lng: 141.1193}
+    center: { lat: 38.6306, lng: 141.1193 },
   });
 
- var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
- var markers = locations.map(function(location, i) {
-         return new google.maps.Marker({
-         position: location,
-         label: labels[i % labels.length]   
-        });
- });
+  var markers = locations.map(function (location, i) {
+    return new google.maps.Marker({
+      position: location,
+      label: labels[i % labels.length],
+    });
+  });
 
- var markerCluster = new MarkerClusterer(map, markers,
-          {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
-          
- var locations = [
-     {marker: {lat: 38.1365, lng: 140.4495}, description: "Kurikomayama"}, 
-     {marker: {lat: 38.9608, lng: 140.7882}, description: "Naruko Gorge"},
-     {marker: {lat: 38.7296, lng: 140.6908}, description: "Zao Okama"},
-     {marker: {lat: 38.5889, lng: 141.0984}, description: "Kagobo Park Golf Course"},
-     {marker: {lat: 38.4717, lng: 140.8864}, description: "Manyo Create Park"},
-     {marker: {lat: 38.1853, lng: 140.6719}, description: "Michinoku Park"},
-     {marker: {lat: 38.1267, lng: 140.5287}, description: "Onikuobe"},
-     {marker: {lat: 38.7869, lng: 140.6394}, description: "Spring Valley"},
-     {marker: {lat: 38.4188, lng: 140.7230}, description: "Zao Eboshi Ski Resort"}
-];
-
-
-   // infoWindows 
-   var  infowindow = new google.maps.InfoWindow();
-
- //  var request = {
-  //  location: 'location',
- //   fields: ['name', 'geometry'],
- // };
-
-   var service = new google.maps.places.PlacesService(map);
-
-       service.findPlaceFromQuery(request, function(results, status) {
-            if (status === google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < results.length; i++) {
-                createMarker(results[i]);
-                 }
-               map.setCenter(results[0].geometry.location);
-            }
-        });
-
-    // suggestion from my mentor Rohit Sharma  
-   function createMarker(place) {
-      var markers = locations.map(function(location, i) {
-          marker =  new google.maps.Marker({
-            position: location.marker,
-            label: labels[i % labels.length]
-          });
-
-          google.maps.event.addListener(marker, 'click', function() {
-              infowindow.setContent('<div><strong>' + location.description + '</strong><br></div>');
-              infowindow.open(map, this);
-            });
-          return marker;
-         }); 
-    }
-
-    // marker clustering by google map
-     
-
-
+  // marker clustering by google map
+  var markerCluster = new MarkerClusterer(map, markers, {
+    imagePath:
+      "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+  });
 }
 
-// Filter table: taken from https://www.w3schools.com/howto/howto_js_filter_table.asp 
+var locations = [
+  { lat: 38.1365, lng: 140.4495 },
+  { lat: 38.9608, lng: 140.7882 },
+  { lat: 38.7296, lng: 140.6908 },
+  { lat: 38.5889, lng: 141.0984 },
+  { lat: 38.4717, lng: 140.8864 },
+  { lat: 38.1853, lng: 140.6719 },
+  { lat: 38.1267, lng: 140.5287 },
+  { lat: 38.7869, lng: 140.6394 },
+  { lat: 38.4188, lng: 140.723 },
+];
+
+// Filter table: taken from https://www.w3schools.com/howto/howto_js_filter_table.asp
 function myFunction() {
   var input, filter, table, tr, td, i, txtValue;
   input = document.getElementById("searchActivities");
@@ -79,14 +41,26 @@ function myFunction() {
   tr = table.getElementsByTagName("tr");
 
   for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[0];
-      if (td) {
-         txtValue = td.textContent || td.innerText;
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
         tr[i].style.display = "";
-        } else {
+      } else {
         tr[i].style.display = "none";
-       }
+      }
     }
   }
+}
+
+// taken from w3schools.com/how to add active class to current element
+
+var header = document.getElementById("navbarNavDropdown");
+var btns = header.getElementsByClassName("nav-link");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
 }
